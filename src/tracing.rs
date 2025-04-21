a74b4091c4dd254d1e7f674f92d60d27556050f1
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-21 10:23:58 +0100

    Update tracing.rs

--- src/tracing.rs
+++ src/tracing.rs
@@ -4,15 +4,27 @@ use alloy::providers::ext::DebugApi;
 use alloy::providers::Provider;
 use alloy::rpc::types::trace::common::TraceResult;
 use alloy::rpc::types::trace::geth::{
-    GethDebugBuiltInTracerType::PreStateTracer,
-    GethDebugTracerType::BuiltInTracer
+    GethTrace,               // Needed for matching against `GethTrace::PreStateTracer`
+    PreStateConfig,          // Needed for `.with_prestate_config()`
+    GethDebugTracingOptions, // Used to configure the tracing options
+    GethDefaultTracingOptions,
+    PreStateFrame,           // To destructure frame type from the trace result
+    AccountState             // Type used to track storage diffs
 };
+
 use alloy::rpc::types::trace::geth;
 use alloy::rpc::types::BlockNumberOrTag;
 use alloy::transports::Transport;
 use log::warn;
 use std::collections::BTreeMap;
 use std::sync::Arc;
+
+
+#[derive(Debug)]
+pub struct Application {
+    event_loop: EventLoop<()>,
+    window: Window
+}
 
 // Trace the block to get all addresses with storage changes
 pub async fn debug_trace_block<T: Transport + Clone, N: Network, P: Provider<T, N>>(
