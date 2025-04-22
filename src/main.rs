ee6396b1f126be4f5ef9975cb64335d4f57f8cbe
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-22 23:21:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use alloy::{
-    alloy-eips::{BlockId, BlockNumberOrTag},
-    alloy-primitives::U256,
+    eips::{BlockId, BlockNumberOrTag},
+    primitives::U256,
 };
 use anyhow::Result;
 use ignition::start_workers;
