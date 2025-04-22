a769cff109f16e7b12f618eaabd5c591e297307b
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-22 11:26:40 +0100

    Update graph.rs

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,5 @@
 use crate::swap::{SwapPath, SwapStep};
-use alloy::primitives::Address;
+use alloy::alloy-primitives::Address;
 use petgraph::graph::{NodeIndex, UnGraph};
 use petgraph::prelude;
 use pool_sync::{UniswapV2, UniswapV3, SushiswapV2, SushiswapV3, PancakeswapV2, PancakeswapV3, BalancerV2Pool, BaseswapV2, BaseswapV3, MaverickV1, MaverickV2, Aerodrome, Slipstream, AlienBase, CurveTriCryptoPool, Pool, PoolSync, PoolType, Chain, PoolInfo};
