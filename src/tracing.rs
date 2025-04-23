a8a7f329e6508812c39ef72c8749df9089244baf
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-23 03:39:33 +0100

    Update tracing.rs

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,11 +1,11 @@
 use alloy::{
     consensus::Transaction,
     network::TransactionBuilder,
-    primitives::{address, U256},
-    providers::{Provider, ProviderBuilder},
+    primitives::{ address, U256 },
+    providers::{ Provider, ProviderBuilder },
     rpc::types::request::TransactionRequest,
     rpc::types::{
-        trace::geth::{GethDebugTracingCallOptions, Bundle, StateContext, TransactionRequest, GethTrace, GethDebugTracerType, GethDebugBuiltInTracerType, PreStateConfig, GethDebugTracingOptions, GethDefaultTracingOptions, PreStateFrame, AccountState
+        trace::geth::{ GethDebugTracingCallOptions, Bundle, StateContext, TransactionRequest, GethTrace, GethDebugTracerType, GethDebugBuiltInTracerType, PreStateConfig, GethDebugTracingOptions, GethDefaultTracingOptions, PreStateFrame, AccountState
     },
 };
 use log::warn;
