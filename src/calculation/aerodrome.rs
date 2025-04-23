d586e3b9ac7370b7838f1fcb4679e842060a6980
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-23 02:55:42 +0100

    Update aerodrome.rs

--- src/calculation/aerodrome.rs
+++ src/calculation/aerodrome.rs
@@ -1,10 +1,22 @@
 use super::Calculator;
-use alloy::sol;
-use alloy::alloy-network ::Network;
-use alloy::alloy-primitives::Address;
-use alloy::alloy-primitives::U256;
-use alloy::alloy-provider::Provider;
-use alloy::alloy-transport::Transport;
+use alloy::{
+    consensus::Transaction,
+    network::{TransactionBuilder, EthereumWallet, Ethereum, Network, BlockResponse, HeaderResponse}
+    primitives::{BlockNumber, B256, I256, hex, address, U256, U160, Address, FixedBytes, Bytes},
+    providers::{Provider, ProviderBuilder, RootProvider},
+      rpc::client::RpcClient,
+    transports::http::{
+        reqwest::{
+            header::{HeaderMap, HeaderValue, AUTHORIZATION},
+            Client,
+        },
+        Http,
+      Transport,
+      TransportError 
+    },
+ sol,
+ sol_types::{SolCall, SolValue, SolType},
+};
 
 sol! {
     #[sol(rpc)]
