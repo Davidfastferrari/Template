e57612a97c4689de2b331b7836e8272515bdcaec
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-23 01:32:14 +0100

    Update stream.rs

--- src/stream.rs
+++ src/stream.rs
@@ -1,5 +1,5 @@
 use crate::events::Event;
-use alloy::providers::{Provider, ProviderBuilder, IpcConnect};
+use alloy::providers::{IpcConnect, Provider, ProviderBuilder};
 use futures::StreamExt;
 use log::{debug, warn};
 use tokio::sync::broadcast::Sender;
