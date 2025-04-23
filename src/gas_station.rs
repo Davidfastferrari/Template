5dda1f0ba299a6c1948f0f12d6eda4ce5442f85c
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-23 02:43:31 +0100

    Update gas_station.rs

--- src/gas_station.rs
+++ src/gas_station.rs
@@ -1,9 +1,29 @@
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering;
 use tokio::sync::broadcast::Receiver;
-use alloy::alloy-primitives::U256;
-use alloy::alloy-eips::eip1559::BaseFeeParams;
-use alloy::alloy-eips::calc_next_block_base_fee;
+use alloy::{
+    eips::{BlockId, Encodable2718, calc_next_block_base_fee, eip1559::{BaseFeeParams}),
+    consensus::Transaction,
+    network::{TransactionBuilder, EthereumWallet, Ethereum, Network}
+    primitives::{hex, address, U256, U160, Address, FixedBytes, Bytes},
+    providers::{Provider, ProviderBuilder, RootProvider},
+    rpc::types::{TransactionRequest, BlockNumberOrTag),
+    rpc::types::{
+        trace::geth::GethDebugTracingCallOptions, Bundle, StateContext, TransactionRequest, GethTrace, GethDebugTracerType, GethDebugBuiltInTracerType, PreStateConfig, GethDebugTracingOptions, GethDefaultTracingOptions, PreStateFrame, AccountState
+    },
+   signer::local::PrivateKeySigner,
+   signer::k256::SecretKey,
+      rpc::client::RpcClient,
+    transports::http::{
+        reqwest::{
+            header::{HeaderMap, HeaderValue, AUTHORIZATION},
+            Client,
+        },
+        Http,
+    },
+    sol,
+    sol_types::{SolCall, SolValue, SolType},
+};
 use events::Event;
 
 // Handles all gas state and calculations
