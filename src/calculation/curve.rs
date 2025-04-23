7c61d9b33aefe2cc5a85af3e8cb6e1dc6da19bd5
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-23 02:54:08 +0100

    Update curve.rs

--- src/calculation/curve.rs
+++ src/calculation/curve.rs
@@ -1,7 +1,22 @@
 use super::Calculator;
-use alloy::alloy-primitives::{Address, address};
-use alloy::sol;
-use alloy::primitives::U256;
+use alloy::{
+    consensus::Transaction,
+    network::{TransactionBuilder, EthereumWallet, Ethereum, Network, BlockResponse, HeaderResponse}
+    primitives::{BlockNumber, B256, I256, hex, address, U256, U160, Address, FixedBytes, Bytes},
+    providers::{Provider, ProviderBuilder, RootProvider},
+      rpc::client::RpcClient,
+    transports::http::{
+        reqwest::{
+            header::{HeaderMap, HeaderValue, AUTHORIZATION},
+            Client,
+        },
+        Http,
+      Transport,
+      TransportError 
+    },
+ sol,
+ sol_types::{SolCall, SolValue, SolType},
+};
 use revm::primitives::{ExecutionResult, TransactTo};
 use alloy::sol_types::{SolCall, SolValue, SolStruct, SolInterface};
 use revm::Evm;
