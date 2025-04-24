aeb0d5df0b741922132508b0abeba284e27ba44f
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-24 23:50:56 +0100

    Update calculator.rs

--- Template/src/calculation/calculator.rs
+++ Template/src/calculation/calculator.rs
@@ -1,28 +1,14 @@
-use alloy::{
-    consensus::Transaction,
-    network::{ TransactionBuilder, EthereumWallet, Ethereum, Network, BlockResponse, HeaderResponse },
-    primitives::{ BlockNumber, B256, I256, hex, address, U256, U160, Address, FixedBytes, Bytes },
-    providers::{ Provider, ProviderBuilder, RootProvider },
-      rpc::client::RpcClient,
-    transports::http::{
-        reqwest::{
-            header::{HeaderMap, HeaderValue, AUTHORIZATION},
-            Client,
-        },
-        Http,
-      Transport,
-      TransportError 
-    },
- sol,
- sol_types::{SolCall, SolValue, SolType},
-};
+use alloy::network::Network;
+use alloy::primitives::{Address, U256};
+use alloy::providers::Provider;
+use alloy::transports::Transport;
 use pool_sync::PoolType;
 use std::collections::HashSet;
 use std::sync::Arc;
 
 use crate::cache::Cache;
 use crate::market_state::MarketState;
-use crate::swap::SwapPath;
+use crate::swap::*;
 use crate::AMOUNT;
 
 // Calculator for getting the amount out
