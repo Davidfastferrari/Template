5ef92a511a7e2db446f003fc466740c12e7b6076
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-24 23:50:20 +0100

    Update balancer.rs

--- Template/src/calculation/balancer.rs
+++ Template/src/calculation/balancer.rs
@@ -1,24 +1,7 @@
-use std::ops::Neg;
-use alloy::{
-    consensus::Transaction,
-    network::{ TransactionBuilder, EthereumWallet, Ethereum, Network, BlockResponse, HeaderResponse },
-    primitives::{ BlockNumber, B256, I256, hex, address, U256, U160, Address, FixedBytes, Bytes },
-    providers::{ Provider, ProviderBuilder, RootProvider },
-      rpc::client::RpcClient,
-    transports::http::{
-        reqwest::{
-            header::{ HeaderMap, HeaderValue, AUTHORIZATION },
-            Client,
-        },
-        Http,
-      Transport,
-      TransportError 
-    },
- sol,
- sol_types::{SolCall, SolValue, SolType},
-};
-use std::str::FromStr;
 use super::Calculator;
+use alloy::primitives::{Address, I256, U256};
+use std::ops::Neg;
+use std::str::FromStr;
 
 impl<T, N, P> Calculator<T, N, P>
 where
