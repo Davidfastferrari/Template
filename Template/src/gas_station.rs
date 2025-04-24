66f12ddcbde583d0e02ea3d31d98409bfa92b401
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-24 09:48:33 +0100

    Update gas_station.rs

--- Template/src/gas_station.rs
+++ Template/src/gas_station.rs
@@ -24,7 +24,8 @@ use alloy::{
     sol,
     sol_types::{SolCall, SolValue, SolType},
 };
-crate::events::Event;
+
+use crate::events::Event;
 
 // Handles all gas state and calculations
 pub struct GasStation {
