70a0284bbe23b83f7845fcc309a047aace32c72c
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-25 10:18:36 +0100

    Update calculator.rs

--- Template/src/calculation/calculator.rs
+++ Template/src/calculation/calculator.rs
@@ -8,10 +8,30 @@ use std::sync::Arc;
 
 use crate::cache::Cache;
 use crate::market_state::MarketState;
-use crate::swap::*;
+use crate::swap::{SwapPath, SwapStep};
 use crate::AMOUNT;
 
-// Calculator for getting the amount out
+fn uniswap_v2_out(&self, input: U256, pool: &Address, token: &Address, fee: U256) -> U256 {
+    // TODO: Actual implementation
+    U256::ZERO
+}
+
+fn uniswap_v3_out(&self, input: U256, pool: &Address, token: &Address, fee: u32) -> Option<U256> {
+    // TODO: Actual implementation
+    Some(U256::ZERO)
+}
+
+fn aerodrome_out(&self, input: U256, token: Address, pool: Address) -> U256 {
+    // TODO: Actual implementation
+    U256::ZERO
+}
+
+fn balancer_v2_out(&self, input: U256, token_in: Address, token_out: Address, pool: Address) -> U256 {
+    // TODO: Actual implementation
+    U256::ZERO
+}
+
+/// Calculator handles swap output computations across supported AMM types.
 pub struct Calculator<T, N, P>
 where
     T: Transport + Clone,
@@ -28,8 +48,7 @@ where
     N: Network,
     P: Provider<T, N>,
 {
-    // construct a new calculator
-    // contains the market state to access pool info and a cache for calculations
+    /// Create a new calculator instance with market state and internal cache.
     pub fn new(market_state: Arc<MarketState<T, N, P>>) -> Self {
         Self {
             market_state,
@@ -37,21 +56,19 @@ where
         }
     }
 
-    // calculate the output amount
-    // we can get read access to the db since we know it will not change for duration of calculation
-    #[inline]
+    /// Perform output amount calculation for a given swap path
+    #[inline(always)]
     pub fn calculate_output(&self, path: &SwapPath) -> U256 {
         let mut amount = *AMOUNT;
 
-        // for each step, calculate the amount out
         for swap_step in &path.steps {
             let pool_address = swap_step.pool_address;
 
-            // check to see if we have a up to date cache
+            // Fast path: use cached result
             if let Some(cached_amount) = self.cache.get(amount, pool_address) {
                 amount = cached_amount;
             } else {
-                // compute the output amount and then store it in cache
+                // Compute fresh and store in cache
                 let output_amount = self.compute_amount_out(
                     amount,
                     pool_address,
@@ -59,51 +76,52 @@ where
                     swap_step.protocol,
                     swap_step.fee,
                 );
+
                 self.cache.set(amount, pool_address, output_amount);
                 amount = output_amount;
             }
 
-            if amount == U256::ZERO {
-               return U256::ZERO;
+            if amount.is_zero() {
+                return U256::ZERO;
             }
         }
 
-        // all good, return the output amount of the path
         amount
     }
 
+    /// Return a debug trace of intermediate amounts at each swap step
     pub fn debug_calculation(&self, path: &SwapPath) -> Vec<U256> {
-        let mut path_calc: Vec<U256> = Vec::new();
         let mut amount = *AMOUNT;
-        path_calc.push(amount);
+        let mut path_trace = vec![amount];
 
         for swap_step in &path.steps {
-            let pool_address = swap_step.pool_address;
             let output_amount = self.compute_amount_out(
                 amount,
-                pool_address,
+                swap_step.pool_address,
                 swap_step.token_in,
                 swap_step.protocol,
                 swap_step.fee,
             );
-            path_calc.push(output_amount);
+            path_trace.push(output_amount);
             amount = output_amount;
         }
 
-        path_calc
+        path_trace
     }
 
-    pub fn compute_pool_output(&self, pool_addr: Address, token_in: Address, protocol: PoolType, fee: u32, input: U256) -> U256 {
-        self.compute_amount_out(
-            input,
-            pool_addr,
-            token_in,
-            protocol,
-            fee
-        )
+    /// Calculate pool output with arguments unwrapped from path logic
+    pub fn compute_pool_output(
+        &self,
+        pool_addr: Address,
+        token_in: Address,
+        protocol: PoolType,
+        fee: u32,
+        input: U256,
+    ) -> U256 {
+        self.compute_amount_out(input, pool_addr, token_in, protocol, fee)
     }
 
-    // calculate the ratio for the pool
+    /// Main dispatcher for computing output amount based on AMM type
     pub fn compute_amount_out(
         &self,
         input_amount: U256,
@@ -129,20 +147,75 @@ where
             | PoolType::PancakeSwapV3
             | PoolType::AlienBaseV3
             | PoolType::SwapBasedV3
-            | PoolType::DackieSwapV3 => self
-                .uniswap_v3_out(input_amount, &pool_address, &token_in, fee)
-                .unwrap(),
+            | PoolType::DackieSwapV3 => {
+                self.uniswap_v3_out(input_amount, &pool_address, &token_in, fee)
+                    .expect("Uniswap V3 computation failed")
+            }
             PoolType::Aerodrome => self.aerodrome_out(input_amount, token_in, pool_address),
-            PoolType::MaverickV1 | PoolType::MaverickV2 => todo!(),
-            PoolType::BalancerV2 => todo!(),
-            PoolType::CurveTwoCrypto | PoolType::CurveTriCrypto => todo!()
+            PoolType::BalancerV2 => self.balancer_v2_out(input_amount, token_in, token_in, pool_address),
+            PoolType::MaverickV1 | PoolType::MaverickV2 => {
+                tracing::warn!("Maverick pool logic not implemented");
+                U256::ZERO
+            }
+            PoolType::CurveTwoCrypto | PoolType::CurveTriCrypto => {
+                tracing::warn!("Curve pool logic not implemented");
+                U256::ZERO
+            }
         }
     }
 
-    #[inline]
+    /// Invalidate cache for specific pools (e.g. after state mutation)
+    #[inline(always)]
     pub fn invalidate_cache(&self, updated_pools: &HashSet<Address>) {
         for pool in updated_pools {
             self.cache.invalidate(*pool)
+        }
+    }
+}
+
+impl<T, N, P> Calculator<T, N, P>
+where
+    T: Transport + Clone,
+    N: Network,
+    P: Provider<T, N>,
+{
+    /// Simulate sandwich backrun MEV strategy
+    ///
+    /// A -> B (user trade) -> A' (MEV backrun)
+    pub fn simulate_sandwich_mev(
+        &self,
+        frontrun: &SwapPath,
+        victim: &SwapPath,
+        backrun: &SwapPath,
+    ) -> (U256, U256, U256) {
+        let front_output = self.calculate_output(frontrun);
+        let victim_output = self.calculate_output(victim);
+        let back_output = self.calculate_output(backrun);
+
+        let total_profit = back_output.saturating_sub(frontrun.steps.first().map(|s| *AMOUNT).unwrap_or_default());
+
+        (front_output, victim_output, total_profit)
+    }
+
+    /// Generate backrun path from a base path by flipping it
+    pub fn reverse_path(&self, original: &SwapPath) -> SwapPath {
+        let mut steps = original.steps.clone();
+        steps.reverse();
+
+        let reversed_steps = steps
+            .into_iter()
+            .map(|step| SwapStep {
+                pool_address: step.pool_address,
+                token_in: step.token_out,
+                token_out: step.token_in,
+                protocol: step.protocol,
+                fee: step.fee,
+            })
+            .collect();
+
+        SwapPath {
+            steps: reversed_steps,
+            hash: original.hash,
         }
     }
 }
