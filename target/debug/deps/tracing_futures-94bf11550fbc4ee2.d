e422c39f7f747aab20528669511157229afb7b28
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 12:05:30 +0100

    fix

---
+++ target/debug/deps/tracing_futures-94bf11550fbc4ee2.d
@@ -0,0 +1,8 @@
+C:\Users\Damulak\Template\target\debug\deps\libtracing_futures-94bf11550fbc4ee2.rmeta: C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\lib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\stdlib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\mod.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\futures_03.rs
+
+C:\Users\Damulak\Template\target\debug\deps\tracing_futures-94bf11550fbc4ee2.d: C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\lib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\stdlib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\mod.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\futures_03.rs
+
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\lib.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\stdlib.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\mod.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-futures-0.2.5\src\executor\futures_03.rs:
