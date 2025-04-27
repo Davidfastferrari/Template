060e4dcc57f823b4b154abeb8ffa7ae95f202760
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 07:12:37 +0100

    Create node_db.rs

---
+++ Template/src/node_db.rs
@@ -0,0 +1,32 @@
+use reth_db::{mdbx::{Env, EnvironmentKind, WriteMap}, DatabaseEnv};
+use reth_db::tables;
+use reth_db::Table;
+use std::path::Path;
+use anyhow::Result;
+
+pub struct NodeDB {
+    env: DatabaseEnv<WriteMap>,
+}
+
+impl NodeDB {
+    pub fn open(path: impl AsRef<Path>) -> Result<Self> {
+        let env = DatabaseEnv::<WriteMap>::open(
+            path,
+            reth_db::mdbx::EnvironmentFlags::empty(),
+            None,
+        )?;
+
+        Ok(Self { env })
+    }
+
+    pub fn env(&self) -> &DatabaseEnv<WriteMap> {
+        &self.env
+    }
+}
+
+// Example type your code was using
+#[derive(Debug, Clone, Copy)]
+pub enum InsertionType {
+    Replace,
+    Merge,
+}
