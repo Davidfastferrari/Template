f995a6fb5e38ca674df74c85d00d5b171d27502c
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 05:33:21 +0100

    Update market_state.rs

--- Template/src/market_state.rs
+++ Template/src/market_state.rs
@@ -81,78 +81,8 @@ where
         Ok(market_state)
     }
 
-    async fn state_updater(
-        self: Arc<Self>,
-        mut block_rx: Receiver<Event>,
-        address_tx: Sender<Event>,
-        mut last_synced_block: u64,
-        caught_up: Arc<AtomicBool>,
-    ) {
-        let http_url = std::env::var("FULL").unwrap(); // assumed validated externally
-        let http = Arc::new(ProviderBuilder::new().on_http(http_url.parse().unwrap()));
+}
 
-        let mut current_block = http.get_block_number().await.unwrap();
-
-        while last_synced_block < current_block {
-            debug!("Catching up from {} to {}", last_synced_block, current_block);
-            for block_num in (last_synced_block + 1)..=current_block {
-                let _ = self.update_state(http.clone(), block_num).await;
-            }
-            last_synced_block = current_block;
-            current_block = http.get_block_number().await.unwrap();
-        }
-        
-        caught_up.store(true, Ordering::Relaxed);
-        while let Some(Event::NewBlock(block_header)) = block_rx.recv().await {
-            let start = Instant::now();
-            let block_number = block_header.inner.number;
-
-            if block_number <= last_synced_block {
-                debug!("Skipping duplicate block {}", block_number);
-                continue;
-            }
-
-            info!("New block received: {}", block_number);
-            let updated = self.update_state(http.clone(), block_number).await;
-
-             if let Err(e) = address_tx.send(Event::PoolsTouched(updated.clone(), block_number)).await {
-                error!("Error sending updates: {}", e);
-            } else {
-                info!("Block {} processed in {:?}", block_number, start.elapsed());
-            }
-
-            last_synced_block = block_number;
-        }
-    }
-
-    async fn update_state(
-        &self,
-        provider: Arc<RootProvider<Http<Client>>>,
-        block_num: u64,
-    ) -> HashSet<Address> {
-        let mut updated_pools = HashSet::new();
-        let updates = debug_trace_block(provider, BlockNumberOrTag::Number(block_num), true).await;
-
-        let mut db = self.db.write().unwrap();
-        for (addr, state) in updates.iter().flat_map(|map| map.iter()) {
-            if db.tracking_pool(addr) {
-                db.update_all_slots(*addr, state.clone()).unwrap();
-                updated_pools.insert(*addr);
-            }
-        }
-
-        updated_pools
-    }
-
-    fn populate_db_with_pools(pools: Vec<Pool>, db: &mut BlockStateDB<N, P>) {
-        for pool in pools {
-            if pool.is_v2() {
-                db.insert_v2(pool);
-            } else if pool.is_v3() {
-                db.insert_v3(pool).unwrap();
-            }
-        }
-    }
 
     fn warm_up_database(pools: &[Pool], db: &mut BlockStateDB<N, P>) {
         let account = address!("d8da6bf26964af9d7eed9e03e53415d37aa96045");
@@ -210,4 +140,78 @@ where
             evm.transact().unwrap();
         }
     }
-}
+
+ async fn state_updater(
+        self: Arc<Self>,
+        mut block_rx: Receiver<Event>,
+        address_tx: Sender<Event>,
+        mut last_synced_block: u64,
+        caught_up: Arc<AtomicBool>,
+    ) {
+        let http_url = std::env::var("FULL").unwrap(); // assumed validated externally
+        let http = Arc::new(ProviderBuilder::new().on_http(http_url.parse().unwrap()));
+
+        let mut current_block = http.get_block_number().await.unwrap();
+
+        while last_synced_block < current_block {
+            debug!("Catching up from {} to {}", last_synced_block, current_block);
+            for block_num in (last_synced_block + 1)..=current_block {
+                let _ = self.update_state(http.clone(), block_num).await;
+            }
+            last_synced_block = current_block;
+            current_block = http.get_block_number().await.unwrap();
+        }
+        
+        caught_up.store(true, Ordering::Relaxed);
+        while let Some(Event::NewBlock(block_header)) = block_rx.recv().await {
+            let start = Instant::now();
+            let block_number = block_header.inner.number;
+
+            if block_number <= last_synced_block {
+                debug!("Skipping duplicate block {}", block_number);
+                continue;
+            }
+
+            info!("New block received: {}", block_number);
+            let updated = self.update_state(http.clone(), block_number).await;
+
+             if let Err(e) = address_tx.send(Event::PoolsTouched(updated.clone(), block_number)).await {
+                error!("Error sending updates: {}", e);
+            } else {
+                info!("Block {} processed in {:?}", block_number, start.elapsed());
+            }
+
+            last_synced_block = block_number;
+        }
+    }
+
+  fn populate_db_with_pools(pools: Vec<Pool>, db: &mut BlockStateDB<N, P>) {
+        for pool in pools {
+            if pool.is_v2() {
+                db.insert_v2(pool);
+            } else if pool.is_v3() {
+                db.insert_v3(pool).unwrap();
+            }
+        }
+    }
+
+
+    async fn update_state(
+        &self,
+        provider: Arc<RootProvider<Http<Client>>>,
+        block_num: u64,
+    ) -> HashSet<Address> {
+        let mut updated_pools = HashSet::new();
+        let updates = debug_trace_block(provider, BlockNumberOrTag::Number(block_num), true).await;
+
+        let mut db = self.db.write().unwrap();
+        for (addr, state) in updates.iter().flat_map(|map| map.iter()) {
+            if db.tracking_pool(addr) {
+                db.update_all_slots(*addr, state.clone()).unwrap();
+                updated_pools.insert(*addr);
+            }
+        }
+
+        updated_pools
+    }
+
