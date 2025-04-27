4a3836fcb63ff6ccd628ea6bc7f6ef822728998b
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 11:17:28 +0100

    line1

---
+++ target/debug/deps/impl_trait_for_tuples-81d6e49b8b816600.d
@@ -0,0 +1,8 @@
+C:\Users\Damulak\Template\target\debug\deps\impl_trait_for_tuples-81d6e49b8b816600.dll: C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\lib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\full_automatic.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\semi_automatic.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\utils.rs
+
+C:\Users\Damulak\Template\target\debug\deps\impl_trait_for_tuples-81d6e49b8b816600.d: C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\lib.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\full_automatic.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\semi_automatic.rs C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\utils.rs
+
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\lib.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\full_automatic.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\semi_automatic.rs:
+C:\Users\Damulak\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\impl-trait-for-tuples-0.2.3\src\utils.rs:
---
+++ target/debug/deps/reth_tracing-c3650d58b9c55409.d
@@ -0,0 +1,8 @@
+C:\Users\Damulak\Template\target\debug\deps\libreth_tracing-c3650d58b9c55409.rmeta: C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\lib.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\formatter.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\layers.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\test_tracer.rs
+
+C:\Users\Damulak\Template\target\debug\deps\reth_tracing-c3650d58b9c55409.d: C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\lib.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\formatter.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\layers.rs C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\test_tracer.rs
+
+C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\lib.rs:
+C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\formatter.rs:
+C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\layers.rs:
+C:\Users\Damulak\.cargo\git\checkouts\reth-e231042ee7db3fb7\6f8e725\crates\tracing\src\test_tracer.rs:
