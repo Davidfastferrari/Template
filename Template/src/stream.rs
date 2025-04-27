d0d55b050f5505bda1a7d3273a27a38b1fda2c6e
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 02:57:43 +0100

    Update stream.rs

--- Template/src/stream.rs
+++ Template/src/stream.rs
@@ -1,8 +1,9 @@
-use crate::events::Event;
 use alloy::providers::{Provider, ProviderBuilder, Ipc};
 use futures::StreamExt;
 use log::{debug, warn};
 use tokio::sync::broadcast::Sender;
+
+use crate::events::Event;
 
 /// Subscribes to new block headers over IPC and broadcasts them via a channel.
 pub async fn stream_new_blocks(block_sender: Sender<Event>) {
