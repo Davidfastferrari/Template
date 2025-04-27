1681147f87dfc31b31e3c18e2c627fa4186aae56
Davidfastferrari <daudahassan9825@gmail.com>
2025-04-27 05:56:28 +0100

    Create lib.rs

---
+++ Template/src/lib.rs
@@ -0,0 +1,24 @@
+// src/lib.rs
+
+pub mod bytecode;
+pub mod cache;
+pub mod calculation;
+pub mod constants;
+pub mod estimator;
+pub mod events;
+pub mod filter;
+pub mod gas_station;
+pub mod gen;
+pub mod graph;
+pub mod ignition;
+pub mod market_state;
+pub mod quoter;
+pub mod searcher;
+pub mod simulator;
+pub mod state_db;
+pub mod stream;
+pub mod swap;
+pub mod tests;
+pub mod tracing;
+pub mod tx_sender;
+pub mod history_db;
